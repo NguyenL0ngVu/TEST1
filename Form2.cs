ac4276128d778be3f12a2eaf77548c93e8d45b47
NguyenL0ngVu <nguyenlongvu16648@gmail.com>
2025-03-19 09:41:39 +0700

    create button and functions

--- Form2.cs
+++ Form2.cs
@@ -16,5 +16,10 @@ namespace Test1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show ("Error", "You cann't connected", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
     }
 }
