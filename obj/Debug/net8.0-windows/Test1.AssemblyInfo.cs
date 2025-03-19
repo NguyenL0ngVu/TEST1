e13db15fa8e697a479a5c8d2663209f10e51234d
NguyenL0ngVu <nguyenlongvu16648@gmail.com>
2025-03-19 11:00:47 +0700

    create frm5

--- obj/Debug/net8.0-windows/Test1.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Test1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a45af6dbd5365cd9c62cb7d0cdaaf68ccb07f71")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7be5d049284a5bb335cca50a751eb9be62a05da0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
